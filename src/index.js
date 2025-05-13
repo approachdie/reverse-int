9dbd93dbcb18550c97ee11d8b089c40f4b7588b8
Дмитрий Шергин <dmitrishergin@MacBook-Air-Dmitrij.local>
2025-05-13 19:41:48 +0300

    feat: add js code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let number = n.toString();
+    let result = "";
+    if (n > 0) {
+        for (let i = number.length - 1; i >= 0; i--) {
+            result += number[i];
+        }
+    } else {
+        for (let i = number.length - 1; i > 0; i--) {
+            result += number[i];
+        }
+    }
+    return parseInt(result);
+};
